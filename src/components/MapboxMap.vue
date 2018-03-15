1e4f69bd6a2d373f2f009f25c315784dfb170849
Arden de Raaij <a.de.raaij@gmail.com>
2018-03-15 21:02:16 +0000

    [fix] marker click

--- src/components/MapboxMap.vue
+++ src/components/MapboxMap.vue
@@ -2,6 +2,7 @@
   <div>
     <div
       class='mapWrapper'
+      :id="(mapOptions.hasOwnProperty('container') ? mapOptions.container : 'map')"
       :ref="'mapdiv'">
       <div
         v-for="location in locations.features"
@@ -9,6 +10,7 @@
         :position="location.geometry.coordinates"
         :key="location.title"
         :ref="'markers'"
+        :center="center"
         @click="getCenter(location.geometry.coordinates)">
         <map-marker 
           :iconType="location.properties.type"
@@ -22,7 +24,6 @@
 import mapboxgl from 'mapbox-gl';
 import { mapState, mapActions, mapGetters } from 'vuex';
 import MapMarker from './MapMarker';
-import kebabCase from 'lodash.kebabcase';
 export default {
   components: {
     MapMarker,
@@ -37,34 +38,66 @@ export default {
       required: true,
       type: Object,
     },
+    mapOptions: {
+      type: Object,
+      required: true,
+    },
   },
 
   computed: {
     ...mapState(['zoom', 'center']),
   },
 
-  methods: {
-    doClickety: function() {
-      console.log('done clickety!');
-    },
-    ...mapActions(['getCenter']),
+  mounted() {
+    //Initialze Map
+    const map = this.mapInit();
+    //Add Controls to map
+    // this.addControls(map);
+    //Register Map Events
+    this.registerEvents(map);
   },
 
-  mounted() {
-    const map = new mapboxgl.Map({
-      container: this.$refs.mapdiv,
-      style: this.mapStyle,
-      zoom: this.zoom,
-      center: this.center,
-    });
-    const bounds = new mapboxgl.LngLatBounds();
-    this.locations.features.forEach((item, i) => {
-      const markerRef = this.$refs.markers[i];
-      new mapboxgl.Marker(this.$refs.markers[i], { offset: [0, -30] })
-        .setLngLat(item.geometry.coordinates)
-        .addTo(map);
-      bounds.extend(item.geometry.coordinates);
-    });
+  methods: {
+    ...mapActions(['getCenter']),
+    mapInit() {
+      //Add container to options object
+      if (!this.mapOptions.hasOwnProperty('container')) {
+        this.mapOptions.container = 'map';
+      }
+      //New Mapbox Instance
+      const map = new mapboxgl.Map(this.mapOptions);
+      //Emit init event passing map object
+      this.$emit('map-init', map);
+      return map;
+    },
+    registerEvents(map) {
+      // Load Event
+      map.on('load', () => {
+        // Emit evet usable in parent compontent
+        this.$emit('map-load', map);
+
+        // Add locations
+        this.locations.features.forEach((item, i) => {
+          const markerRef = this.$refs.markers[i];
+          new mapboxgl.Marker(this.$refs.markers[i], { offset: [0, -30] })
+            .setLngLat(item.geometry.coordinates)
+            .addTo(map);
+        });
+      });
+
+      //Map Mouse Move
+      map.on('mousemove', e => {
+        this.$emit('map-mousemove', map, e);
+      });
+
+      //Map Clicked
+      map.on('click', e => {
+        this.$emit('map-click', map, e);
+      });
+    },
+  },
+  updated() {
+    console.log('updated');
   },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,20 @@
       v-if="!loading"
       :mapStyle="mapStyle"
       :locations="locations"
+      :map-options="{
+        style: mapStyle,
+        center: [-96, 37.8],
+        zoom: 3
+      }"
+      @map-load="mapLoaded"
+      @map-click="mapClicked"
     />
     <p v-else>loading</p>
   </div>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
+import { mapState, mapGetters, mapActions } from 'vuex';
 import MapboxMap from '@/components/MapboxMap';
 
 export default {
@@ -20,9 +27,19 @@ export default {
   },
   computed: {
     ...mapState(['loading', 'mapStyle', 'locations']),
+    ...mapGetters(['mapCenter']),
   },
   methods: {
     ...mapActions(['getMapStyle', 'getLocations', 'toggleLoading']),
+    mapLoaded(map) {
+      console.log(map);
+    },
+    mapClicked(map, e) {
+      map.flyTo({
+        center: this.mapCenter,
+        zoom: 11,
+      });
+    },
   },
   mounted() {
     Promise.all([this.getMapStyle(), this.getLocations()]).then(() => {
