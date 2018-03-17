07a6007810a521604a55041058a80c2f3c94b244
Arden de Raaij <a.de.raaij@gmail.com>
2018-03-17 18:31:27 +0000

    [create] seperate types file

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import geoData from '@/data/api';
-
-const SET_LOCATIONS = 'SET_LOCATIONS';
-const SET_MAPSTYLE = 'SET_MAPSTYLE';
-
-const TOGGLE_DATA_LOADED = 'TOGGLE_DATA_LOADED';
-const TOGGLE_MAP_LOADED = 'TOGGLE_MAP_LOADED';
-const TOGGLE_LOCATION_HOVER = 'TOGGLE_LOCATION_HOVER';
-
-const SET_CENTER = 'SET_CENTER';
-const SET_EXCERPT_ID = 'SET_EXCERPT_ID';
+import * as types from './types';
 
 const api =
   'https://s3-us-west-2.amazonaws.com/s.cdpn.io/22914/map_dark-matter.json';
@@ -42,7 +33,7 @@ const actions = {
         .then(response => response.json())
         .then(json => {
           resolve();
-          commit(SET_MAPSTYLE, json);
+          commit(types.SET_MAPSTYLE, json);
         });
     });
   },
@@ -50,58 +41,58 @@ const actions = {
   setLocations({ commit }) {
     return new Promise(resolve => {
       setTimeout(() => {
-        commit(SET_LOCATIONS, geoData);
+        commit(types.SET_LOCATIONS, geoData);
         resolve();
       }, 300);
     });
   },
 
   setCenter({ commit }, position) {
-    commit(SET_CENTER, position);
+    commit(types.SET_CENTER, position);
   },
 
   setActiveId({ commit }, id) {
-    commit(SET_CENTER);
+    commit(types.SET_CENTER);
   },
 
   toggleMapLoaded({ state, commit }) {
-    commit(TOGGLE_MAP_LOADED, state.mapLoaded);
+    commit(types.TOGGLE_MAP_LOADED, state.mapLoaded);
   },
 
   toggleDataLoaded({ state, commit }) {
-    commit(TOGGLE_DATA_LOADED, state.dataLoaded);
+    commit(types.TOGGLE_DATA_LOADED, state.dataLoaded);
   },
 
   toggleLocationHover({ state, commit }, position) {
-    commit(TOGGLE_LOCATION_HOVER, state.locationHover);
-    commit(SET_CENTER, position);
+    commit(types.TOGGLE_LOCATION_HOVER, state.locationHover);
+    commit(types.SET_CENTER, position);
   },
 
   setExcerptId({ state, commit }, id) {
-    commit(SET_EXCERPT_ID, id);
+    commit(types.SET_EXCERPT_ID, id);
   },
 };
 
 const mutations = {
-  [SET_MAPSTYLE](state, json) {
+  [types.SET_MAPSTYLE](state, json) {
     state.mapStyle = json;
   },
-  [TOGGLE_DATA_LOADED](state, dataLoaded) {
+  [types.TOGGLE_DATA_LOADED](state, dataLoaded) {
     state.dataLoaded = !dataLoaded;
   },
-  [TOGGLE_MAP_LOADED](state, mapLoaded) {
+  [types.TOGGLE_MAP_LOADED](state, mapLoaded) {
     state.mapLoaded = !mapLoaded;
   },
-  [SET_LOCATIONS](state, geoData) {
+  [types.SET_LOCATIONS](state, geoData) {
     state.locations = geoData;
   },
-  [SET_CENTER](state, center) {
+  [types.SET_CENTER](state, center) {
     state.center = center;
   },
-  [SET_EXCERPT_ID](state, excerptId) {
+  [types.SET_EXCERPT_ID](state, excerptId) {
     state.excerptId = excerptId;
   },
-  [TOGGLE_LOCATION_HOVER](state, locationHover) {
+  [types.TOGGLE_LOCATION_HOVER](state, locationHover) {
     state.locationHover = !locationHover;
   },
 };
