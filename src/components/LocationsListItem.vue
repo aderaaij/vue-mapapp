92785911a935b8d433a12d23803ecf0b59f26a74
Arden de Raaij <a.de.raaij@gmail.com>
2018-03-17 18:32:07 +0000

    [add] list item hover checks

--- src/components/LocationsListItem.vue
+++ src/components/LocationsListItem.vue
@@ -1,17 +1,30 @@
 <template>
-  <div @mouseenter="setActive(location)">
+  <div 
+    @mouseenter="setActive(location)"
+    class="listItem"
+    :class="[checkIfActive ? 'listItem--active' : '']">
     <h3>{{ location.properties.title }}</h3>
     <img :src="location.properties.imageUrl">
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 export default {
   props: {
     location: {
       required: true,
       type: Object,
+    },
+  },
+
+  computed: {
+    ...mapGetters(['getExcerptId']),
+    checkIfActive() {
+      if (this.location.properties.id === this.getExcerptId) {
+        return true;
+      }
+      return false;
     },
   },
 
@@ -25,6 +38,28 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.listItem {
+  & {
+    transition: background 0.3s ease;
+    padding: 2em;
+  }
+
+  &--active {
+    background: #ccc;
+  }
+
+  img {
+    display: block;
+    width: 100%;
+    height: auto;
+  }
+
+  h3 {
+    margin: 0 0 1em;
+  }
+}
+</style>
+
 
 </style>
