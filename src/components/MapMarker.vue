6cd0c9e9d841c9e9b79b5d142d5519db2b3da0c7
Arden de Raaij <a.de.raaij@gmail.com>
2018-03-18 13:03:38 +0000

    [add] title and animations

--- src/components/MapMarker.vue
+++ src/components/MapMarker.vue
@@ -2,8 +2,8 @@
   <div
     class='e-marker'
     :class="[isActive ? 'e-marker--active' : '']">
-    <svg 
-      class="e-marker__icon"
+    <svg
+      class="e-marker__svg"
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
       xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -16,14 +16,14 @@
           <path d="M36,97.4c15,0,27.3-12.2,27.3-27.3c0-15-12.2-27.3-27.3-27.3S8.7,55.1,8.7,70.2S21,97.4,36,97.4z"/>
         </clipPath>
       </defs>
-      <path 
+      <path
         fill="currentColor"
         class="e-marker__marker"
         d="M60.7,45.4C54.1,38.8,45.3,35.2,36,35.2c-9.3,0-18.1,3.6-24.7,10.3C4.6,52,1,60.8,1,70.2c0,6.3,1.5,11.6,4.6,16.7
       C8.4,91.3,12.1,95,16,98.9c7.3,7.2,15.5,15.4,19,30.5c0.1,0.5,0.5,0.8,1,0.8s0.9-0.3,1-0.8c3.5-15.1,11.7-23.3,19-30.5
       c3.9-3.9,7.6-7.6,10.4-12.1c3.1-5.1,4.6-10.3,4.6-16.7C71,60.8,67.4,52,60.7,45.4z M36,97.4c-15,0-27.3-12.2-27.3-27.3
       S21,42.9,36,42.9c15,0,27.3,12.2,27.3,27.3C63.3,85.2,51,97.4,36,97.4z"/>
-      <path 
+      <path
         fill="transparent"
         class="e-marker__circle"
         d="M36,97.4c15,0,27.3-12.2,27.3-27.3c0-15-12.2-27.3-27.3-27.3S8.7,55.1,8.7,70.2S21,97.4,36,97.4z"/>
@@ -33,17 +33,17 @@
         d="M41.1,68.7V58.5L36,53.4l-5.1,5.1v3.4H20.8v23.7h30.5V68.7H41.1z M27.5,82.2h-3.4v-3.4h3.4V82.2z M27.5,75.4h-3.4
     V72h3.4V75.4z M27.5,68.7h-3.4v-3.4h3.4V68.7z M37.7,82.2h-3.4v-3.4h3.4V82.2z M37.7,75.4h-3.4V72h3.4V75.4z M37.7,68.7h-3.4v-3.4
     h3.4V68.7z M37.7,61.9h-3.4v-3.4h3.4V61.9z M47.9,82.2h-3.4v-3.4h3.4V82.2z M47.9,75.4h-3.4V72h3.4V75.4z"/>
-      <path 
+      <path
         v-else-if="iconType === 'beach'"
         class="e-marker__icon e-marker__icon--beach"
         d="M38.3,74.8l2.9-2.9L54,84.8l-2.9,2.9L38.3,74.8z M46.8,63.3l5.7-5.7c-7.9-7.9-20.7-7.9-28.6,0C31.8,55,40.6,57.1,46.8,63.3z
       M23.9,57.6c-7.9,7.9-7.9,20.7,0,28.6l5.7-5.7C23.4,74.2,21.3,65.5,23.9,57.6z M23.9,57.6L23.9,57.6c-0.8,6,2.3,13.8,8.6,20.1
       L44,66.2C37.7,59.9,30,56.8,23.9,57.6z"/>
-      <path 
+      <path
         v-else-if="iconType === 'mountain'"
         class="e-marker__icon e-marker__icon--mountain"
         d="M39.5,58.8l-6.6,8.8l5,6.7l-2.8,2.1c-3-3.9-7.9-10.5-7.9-10.5l-10.5,14h38.6L39.5,58.8z"/>
-      <g 
+      <g
         v-else-if="iconType === 'jungle'"
         class="e-marker__icon e-marker__icon--jungle">
         <path d="M50.1,81.9c-1.7-2.8-2.8-6.1-5.6-8c1.6-0.2,3.2,0.1,4.6-0.5c2.3-0.9,3.2-3.9,2.7-6.3c-0.5-2.5-2.1-4.5-3.7-6.5
@@ -59,18 +59,36 @@
       M33.2,71.4c-0.5,0.3-1.1,0.4-1.6,0.6c-0.8,0.2-1.5,0.3-2.3,0.3c1.6-0.7,3.2-1.3,4.8-1.9h-0.1c0.1-0.1,0.3,0,0.4,0.1
       C34.2,71,33.7,71.2,33.2,71.4z"/>
       </g>
-      <!-- <image class="e-marker__image" width="100%" height="100%" clip-path="url(#circle)" xlink:href="${imageUrl}" /> -->
-      <!-- <text class="e-marker__text" transform="matrix(1 0 0 1 0 13.9998)">
-          <tspan x="50%" y="0" >${titleFirst.join(' ')}</tspan>
-          <tspan x="50%" y="13" >${titleLast.join(' ')}</tspan>      
-      </text> -->
+      <image
+        opacity="0"
+        class="e-marker__image"
+        width="100%"
+        height="100%"
+        clip-path="url(#circle)"
+        :xlink:href="images.thumb" />
+      <text
+        opacity="0"
+        class="e-marker__text"
+        transform="matrix(1 0 0 1 0 13.9998)">
+          <tspan
+            x="50%"
+            :y="[splitTitle.last ? '0' : '13']" >
+            {{ splitTitle.first }}
+          </tspan>
+          <tspan
+            x="50%"
+            y="13" >
+            {{ splitTitle.last }}
+          </tspan>
+      </text>
     </svg>
   </div>
 </template>
 <script>
 import { mapActions, mapGetters } from 'vuex';
 import pose from 'popmotion-pose';
-import { css, tween, spring, styler, physics } from 'popmotion';
+import { css, tween, spring, styler, svg, physics } from 'popmotion';
+import { titleSplit } from '@/helpers';
 
 export default {
   props: {
@@ -98,11 +116,21 @@ export default {
       return false;
     },
 
+    splitTitle() {
+      return titleSplit(this.location.properties.title);
+    },
+
+    images() {
+      return this.location.properties.images;
+    },
+
     markerPoser() {
       const props = {
         initialPose: 'closed',
         open: {
           scale: 3,
+          delayChildren: 300,
+          staggerChildren: 100,
           transition: ({ from, to }) => {
             return spring({
               from,
@@ -114,7 +142,40 @@ export default {
         },
         closed: { scale: 1 },
       };
-      return pose(this.$el, props);
+
+      const childProps = {
+        icon: {
+          open: { scale: 0 },
+          closed: { scale: 1 },
+        },
+        image: {
+          open: { opacity: 1 },
+          closed: { opacity: 0 },
+        },
+        text: {
+          open: { opacity: 1 },
+          closed: { opacity: 0 },
+        },
+      };
+
+      const markerPose = pose(this.$el, props);
+
+      const iconPose = markerPose.addChild(
+        this.$el.querySelector('.e-marker__icon'),
+        childProps.icon
+      );
+
+      const imagePose = markerPose.addChild(
+        this.$el.querySelector('.e-marker__image'),
+        childProps.image
+      );
+
+      const textPose = markerPose.addChild(
+        this.$el.querySelector('.e-marker__text'),
+        childProps.text
+      );
+
+      return markerPose;
     },
   },
 
@@ -132,18 +193,45 @@ export default {
 <style lang="scss" scoped>
 .e-marker {
   & {
-    fill: #fff;
-    color: #fff;
-    cursor: pointer;
     transform-origin: bottom center;
   }
 
+  &__svg {
+    fill: #fff;
+    color: #fff;
+    cursor: pointer;
+  }
+
   &--active {
-    fill: purple;
+    fill: #ec008c;
   }
 
   svg {
     display: block;
+  }
+
+  &__marker {
+    fill: #fff;
+  }
+
+  &__icon {
+    fill: #fff;
+  }
+
+  &__circle {
+    display: none;
+  }
+
+  &__text {
+    font-size: 12px;
+    font-family: 'Source Sans Pro', sans-serif;
+    font-weight: 700;
+    text-align: center;
+    color: #fff;
+    fill: #fff;
+    transform-origin: 50% 50%;
+    alignment-baseline: middle;
+    text-anchor: middle;
   }
 }
 </style>
