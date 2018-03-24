eea39ea81bcdeaca48663ecb5ee5a0167911ce27
Arden de Raaij <a.de.raaij@gmail.com>
2018-03-24 21:15:21 +0000

    [add] article component

---
+++ src/components/Article.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <article class="m-article">
+      <div class="m-article__bg" :style="`background-image:url('')`">
+      </div>
+      <h1>{{ location.fields.title }}</h1>
+    </article>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+import { tween, styler } from 'popmotion';
+export default {
+  computed: {
+    ...mapGetters(['getLocationsSorted', 'getHoveredLocationId']),
+    location() {
+      if (!this.getLocationsSorted) return;
+      return this.getLocationsSorted.find(
+        item => item.sys.id === this.getHoveredLocationId
+      );
+    },
+  },
+
+  mounted() {
+    console.log(this.location.fields.title);
+  },
+};
+</script>
+<style lang="scss" scoped>
+.m-article {
+  & {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background-color: rgba(#fff, 0.9);
+    height: 100vh;
+    overflow-y: scroll;
+  }
+}
+</style>
+
+
