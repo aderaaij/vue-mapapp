09c76e3f619f36ff68ed58aab13e110225ad85aa
Arden de Raaij <a.de.raaij@gmail.com>
2018-04-25 22:36:37 +0100

    [add] router test

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home';
 import Article from './views/Article';
+import store from './store/index';
+import { stringToSlug } from './helpers';
 
 Vue.use(Router);
 
@@ -14,6 +16,24 @@ export default new Router({
       component: Home,
       children: []
     },
-    { path: '/article/:slug', name: 'Article', component: Article }
+    {
+      path: '/article/:slug',
+      name: 'Article',
+      component: Article,
+      beforeEnter: (to, from, next) => {
+        const { slug } = to.params;
+        store.dispatch('setLocations').then(res => {
+          const currentItem = res.find(
+            item => stringToSlug(item.fields.title).toUpperCase() == slug.toUpperCase()
+          );
+          console.log(currentItem);
+          store.dispatch('setMapCenter', [
+            currentItem.fields.coordinates.lon,
+            currentItem.fields.coordinates.lat
+          ]);
+          next();
+        });
+      }
+    }
   ]
 });
