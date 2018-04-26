69e63d16a0e4bc14f93d21570f2648372fe1534f
Arden de Raaij <a.de.raaij@gmail.com>
2018-04-26 22:52:31 +0100

    [add] copy folder

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,35 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// module.exports = {
+//   lintOnSave: false,
+//   chainWebpack: config => {
+//     config.resolve.alias.set('vue$', 'vue/dist/vue.esm.js');
+//   },
+//   chainWebPack: config =>
+//     new CopyWebpackPlugin([
+//       {
+//         from: path.resolve('./_redirects'),
+//         to: './dist/',
+//         ignore: ['.*']
+//       }
+//     ])
+// };
+
 module.exports = {
-  lintOnSave: false,
-  chainWebpack: config => {
-    config.resolve.alias.set('vue$', 'vue/dist/vue.esm.js');
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin([
+        {
+          from: path.resolve('./copy'),
+          to: './',
+          ignore: ['.*']
+        }
+      ])
+    ],
+    resolve: {
+      alias: {
+        vue$: 'vue/dist/vue.esm.js'
+      }
+    }
   }
 };
